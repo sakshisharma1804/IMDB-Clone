50177013e555b988517ddeae0168b50f194e6add
sakshi sharma <sakshisharma190134@gmail.com>
2025-03-30 02:41:53 +0530

    Initial commit for IMDb Clone project

---
+++ src/constants/constant.js
@@ -0,0 +1,11 @@
+export const logoURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png';
+export const POPULAR_API_URL = 'https://api.themoviedb.org/3/movie/popular?api_key=fb0f7e6e138cde37716f6adf09b2789f';
+export const UPCOMING_API_URL = 'https://api.themoviedb.org/3/movie/upcoming?api_key=fb0f7e6e138cde37716f6adf09b2789f';
+export const TOPRATED_API_URL = 'https://api.themoviedb.org/3/movie/top_rated?api_key=fb0f7e6e138cde37716f6adf09b2789f';
+export const NOWPLAYING_API_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=fb0f7e6e138cde37716f6adf09b2789f';
+
+export const moviesType = {
+upcoming: 'Upcoming',
+toprated: 'Top Rated',
+popular: 'Popular'
+}
